97037de98e4495387fd88180bfb097cb576169bc
iqbalnazir04 <72296513+iqbalnazir04@users.noreply.github.com>
2021-02-15 16:21:28 +0530

    updated

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,51 @@
+import React, {useState, useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
+
+export const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState('');
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount
+    }
+
+    addTransaction(newTransaction);
+    setText('')
+    setAmount('')
+  }
+
+  return (
+    <>
+      <h3>Add New Transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input 
+          type="text" 
+          required
+          value={text} 
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Enter text..." />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount"
+            >Amount </label>
+          <input 
+          type="number" 
+          required
+          value={amount} 
+          onChange={(e) => setAmount(e.target.value)} 
+          placeholder="Enter amount..." />
+        </div>
+        <button className="btn">Add Transaction</button>
+      </form>
+    </>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <h2 className="header">
+      Expense Tracker App by Mirza Iqbal
+    </h2>
+  )
+}
